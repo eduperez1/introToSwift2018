f4dda3824b7c9143cfb42ad7ff2adb6cef058353
klopez <klopez@wayfair.com>
2018-04-24 11:22:43 -0400

    Graded homework 3

---
+++ Assignments/Homework3_EP/Homework3_EP/HomeworkGrade.swift
@@ -0,0 +1,38 @@
+//
+//  HomeworkGrade.swift
+//  Homework3_EP
+//
+//  Created by Kevin Lopez on 4/24/18.
+//  Copyright © 2018 Eduardo Perez Cortes. All rights reserved.
+//
+//
+//
+//  Homework 3
+//
+//  Grade: 98%
+//
+//
+//  A) 10/10
+//
+//  B) 10/10
+//
+//  C) 10/10
+//
+//  D) 8/10: Pay attention to your naming conventions. Enum types begin with a capital letter, which you got right, but each case name should begin with a lowercase letter.
+//
+//  E) 10/10
+//
+//  F) 10/10
+//
+//  G) 10/10
+//
+//  H) 10/10: In following convention please make sure to add a space between () and { when declaring a function
+//
+//  I) 10/10
+//
+//  J) 10/10
+//
+//
+//  Overall: Great job on this homework. I can see you're understanding of the fundamentals is pretty solid. Keep it up Eduardo!
+//
+
